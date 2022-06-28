29ac4c552ae1ffad19e737e66842f620655c3290
Your Name <ssan302@gmail>
2022-06-28 08:58:52 +0700

    first commit

---
+++ JS/coba.js
@@ -0,0 +1,8 @@
+var kalimat = “Hello World”; //string
+var angka = 25; //integer
+var is_true = true; //boolean
+var decimal = 1,2; //double
+var motor = [“Yamaha”, “Honda”, “Suzuki”]; //array
+var biodata = {first_name:”rezky”, last_name:”putra”, age:24} ; //object
+//var digunakan dalam ES < 6 dan dapat menampung variabel dan konstanta biasa//
+//dalam ES > 6 menggunakan const dan Let//
